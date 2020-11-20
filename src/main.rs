421ef0d747c8338a798bcbff66630af8eefd99a1
JohnCorby <williamforgames6@gmail.com>
2020-11-19 22:14:05 -0800

    da path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use walkdir::WalkDir;
+
+/// hardcoded cuz lmao
+const PATH: &str = r"D:\OneDrive - Lake Washington School District\Everything Else\gay\sound is gay\vgm server (ie parker)";
+
 fn main() {
-    println!("Hello, world!");
+    // read all paths recursively, ignoring errors
+    let path_iter = WalkDir::new(PATH)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .map(|entry| entry.into_path())
+        .filter(|path| path.is_file());
+
+    for path in path_iter {
+        println!("{:?}", path);
+    }
 }
