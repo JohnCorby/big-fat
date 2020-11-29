a6be771f320d2d79777dfd95620cf2593d465192
JohnCorby <williamforgames6@gmail.com>
2020-11-28 18:29:40 -0800

    better monitoring. also this method wont work i think

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ mod util;
 
 use crate::audio_reader::AudioReader;
 use crate::audio_result::AudioResult;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::prelude::*;
+use rayon::vec::IntoIter;
+use std::mem::size_of;
+use std::path::PathBuf;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use walkdir::WalkDir;
 
 // config
-const IN_DIR: &str = r"D:\OneDrive - Lake Washington School District\Everything Else\gay\sound is gay\vgm server (ie parker)";
+const IN_DIR: &str =
+    r"D:\OneDrive - Lake Washington School District\Everything Else\gay\sound is gay";
 pub const CHANNELS: u16 = 2;
 pub const SAMPLE_RATE: u32 = 44100;
 pub const OUT_FILE: &str = r".\bruh.wav";
@@ -22,51 +26,41 @@ fn main() {
         .map(|entry| entry.into_path())
         .filter(|path| path.is_file())
         .collect::<Vec<_>>();
-    // artificially extend by duplicating N times
-    // const N: usize = 3;
-    // let total = paths.len();
-    // let paths = paths
-    //     .into_iter()
-    //     .cycle()
-    //     .take(total * N)
-    //     .collect::<Vec<_>>();
 
     let total = paths.len();
-    let count = AtomicUsize::new(1);
+    let num_done = AtomicUsize::new(1);
 
-    let iter: rayon::vec::IntoIter<_> = paths.into_par_iter();
-    // let iter = paths.into_iter();
+    let iter: IntoIter<PathBuf> = paths.into_par_iter();
     let mut audio_results = vec![];
     util::time(|| {
         audio_results = iter
             .fold(
                 || AudioResult::new(),
                 |mut audio_result, path| {
-                    let prefix = format!(
-                        "{}/{} - {:?}",
-                        count.fetch_add(1, Ordering::Relaxed),
-                        total,
-                        path.file_name().unwrap()
-                    );
+                    let name = path.file_name().unwrap().to_str().unwrap();
 
-                    let reader = AudioReader::open(&path);
-                    if reader.is_err() {
-                        println!("{} - error: {:?}", prefix, reader.err().unwrap());
-                        return audio_result;
+                    match AudioReader::open(&path) {
+                        Ok(reader) => {
+                            // process file and track samples
+                            let mut num_samples = 0;
+                            for (j, sample) in reader.enumerate() {
+                                audio_result.add(j, sample);
+                                num_samples = j;
+                            }
+
+                            // print bytes size of samples
+                            println!(
+                                "{} - {} mb",
+                                name,
+                                (num_samples * size_of::<f32>()) as f32 / 1_000_000f32
+                            );
+                        }
+                        Err(error) => println!("{} - error: {:?}", name, error),
                     }
-                    let reader = reader.unwrap();
 
-                    let mut num_samples = 0;
-                    for (index, sample) in reader.enumerate() {
-                        audio_result.add(index, sample);
-                        num_samples = index;
-                    }
-
-                    println!(
-                        "{} - {} mb",
-                        prefix,
-                        (num_samples * std::mem::size_of::<f32>()) as f32 / 1_000_000f32
-                    );
+                    // increment/print done
+                    let num_done = num_done.fetch_add(1, Ordering::Relaxed);
+                    println!("{}/{}", num_done, total);
 
                     audio_result
                 },
